9ce333cb01be73e0f062e5a1d9c467873d673284
jiaxigandragonplus <jiaxi.gan@dragonplus.com>
2026-03-04 18:33:20 +0800

    修改客户端调用

--- src/test/TestClient.ts
+++ src/test/TestClient.ts
@@ -5,28 +5,16 @@
  */
 
 import WebSocket from 'ws';
-import * as protobuf from 'protobufjs';
 import * as path from 'path';
+import * as commonProto from '../../protobuf/js/common/common';
+import * as gameProto from '../../protobuf/js/game/game';
 
 class TestClient {
     private ws: WebSocket;
-    private root: protobuf.Root | null = null;
 
     constructor(url: string) {
         this.ws = new WebSocket(url);
         this.setupHandlers();
-    }
-
-    /**
-     * 加载 proto 文件
-     */
-    async loadProto(): Promise<void> {
-        // __dirname 在编译后是 dist/src/test，需要向上三层到项目根目录
-        const commonProtoPath = path.join(__dirname, '../../../protobuf/proto/common/common.proto');
-        const gameProtoPath = path.join(__dirname, '../../../protobuf/proto/game/game.proto');
-        this.root = await protobuf.load(commonProtoPath);
-        await this.root.load(gameProtoPath);
-        console.log('Proto 文件加载完成');
     }
 
     /**
@@ -55,37 +43,29 @@ class TestClient {
      * 使用 PBPackage 格式解析
      */
     private handleMessage(data: Buffer): void {
-        if (!this.root) {
-            console.error('Proto 文件未加载');
-            return;
-        }
-
         try {
             const uint8Data = new Uint8Array(data);
             
             // 使用 PBPackage 解析
-            const pbPackageType = this.root.lookupType('PBPackage');
-            const pbPackage = pbPackageType.decode(uint8Data) as any;
+            const pbPackage = commonProto.common.PBPackage.decode(uint8Data) as any;
             
-            const messageType = pbPackage.message_type as string;
-            const messagePayload = new Uint8Array(pbPackage.message_payload as ArrayBuffer);
+            const messageType = pbPackage.messageType as string;
+            const messagePayload = new Uint8Array(pbPackage.messagePayload as ArrayBuffer);
             
             console.log(`收到消息 - messageType: ${messageType}`);
             
             // 再次解析 WebsocketMessage
-            const wsMessageType = this.root.lookupType('WebsocketMessage');
-            const wsMessage = wsMessageType.decode(messagePayload) as any;
+            const wsMessage = commonProto.common.WebsocketMessage.decode(messagePayload) as any;
             
-            const innerMessageType = wsMessage.message_type as string;
-            const innerPayload = new Uint8Array(wsMessage.message_payload as ArrayBuffer);
+            const innerMessageType = wsMessage.messageType as string;
+            const innerPayload = new Uint8Array(wsMessage.messagePayload as ArrayBuffer);
             
             console.log(`WebsocketMessage - messageType: ${innerMessageType}`);
             
             // 根据消息类型处理
             switch (innerMessageType) {
                 case 'SLogin':
-                    const sLoginType = this.root.lookupType('SLogin');
-                    const loginResponse = sLoginType.decode(innerPayload) as any;
+                    const loginResponse = gameProto.game.SLogin.decode(innerPayload) as any;
                     console.log(`收到登录响应 - 玩家 ID: ${loginResponse.playerId}`);
                     
                     // 登录成功后发送回显消息
@@ -93,8 +73,7 @@ class TestClient {
                     break;
 
                 case 'SEcho':
-                    const sEchoType = this.root.lookupType('SEcho');
-                    const echoResponse = sEchoType.decode(innerPayload) as any;
+                    const echoResponse = gameProto.game.SEcho.decode(innerPayload) as any;
                     console.log(`收到回显响应 - 消息：${echoResponse.msg}`);
                     
                     // 测试完成，关闭连接
@@ -115,36 +94,28 @@ class TestClient {
      * 使用 WebsocketMessage 格式
      */
     sendLogin(name: string, password: string): void {
-        if (!this.root) {
-            console.error('Proto 文件未加载');
-            return;
-        }
-
         // 编码 CLogin 消息
-        const cLoginType = this.root.lookupType('CLogin');
-        const loginRequest = cLoginType.create({ name, password });
-        const loginEncoded = cLoginType.encode(loginRequest).finish();
+        const loginRequest = gameProto.game.CLogin.create({ name, password });
+        const loginEncoded = gameProto.game.CLogin.encode(loginRequest).finish();
 
         // 创建 WebsocketMessage
-        const wsMessageType = this.root.lookupType('WebsocketMessage');
-        const wsMessage = wsMessageType.create({
+        const wsMessage = commonProto.common.WebsocketMessage.create({
             uri: '/game/game/login',
             method: 'POST',
-            message_type: 'CLogin',
-            message_payload: loginEncoded,
+            messageType: 'CLogin',
+            messagePayload: loginEncoded,
             uuid: '',
             errno: 0,
             errmsg: ''
         });
-        const wsEncoded = wsMessageType.encode(wsMessage).finish();
+        const wsEncoded = commonProto.common.WebsocketMessage.encode(wsMessage).finish();
 
         // 使用 PBPackage 包装
-        const pbPackageType = this.root.lookupType('PBPackage');
-        const pbPackage = pbPackageType.create({
-            message_type: 'CLogin',
-            message_payload: wsEncoded
+        const pbPackage = commonProto.common.PBPackage.create({
+            messageType: 'CLogin',
+            messagePayload: wsEncoded
         });
-        const pbEncoded = pbPackageType.encode(pbPackage).finish();
+        const pbEncoded = commonProto.common.PBPackage.encode(pbPackage).finish();
 
         this.ws.send(pbEncoded);
         console.log(`发送登录请求 - 用户名：${name}`);
@@ -155,36 +126,28 @@ class TestClient {
      * 使用 WebsocketMessage 格式
      */
     sendEcho(msg: string): void {
-        if (!this.root) {
-            console.error('Proto 文件未加载');
-            return;
-        }
-
         // 编码 CEcho 消息
-        const cEchoType = this.root.lookupType('CEcho');
-        const echoRequest = cEchoType.create({ msg });
-        const echoEncoded = cEchoType.encode(echoRequest).finish();
+        const echoRequest = gameProto.game.CEcho.create({ msg });
+        const echoEncoded = gameProto.game.CEcho.encode(echoRequest).finish();
 
         // 创建 WebsocketMessage
-        const wsMessageType = this.root.lookupType('WebsocketMessage');
-        const wsMessage = wsMessageType.create({
+        const wsMessage = commonProto.common.WebsocketMessage.create({
             uri: '/game/game/echo',
             method: 'POST',
-            message_type: 'CEcho',
-            message_payload: echoEncoded,
+            messageType: 'CEcho',
+            messagePayload: echoEncoded,
             uuid: '',
             errno: 0,
             errmsg: ''
         });
-        const wsEncoded = wsMessageType.encode(wsMessage).finish();
+        const wsEncoded = commonProto.common.WebsocketMessage.encode(wsMessage).finish();
 
         // 使用 PBPackage 包装
-        const pbPackageType = this.root.lookupType('PBPackage');
-        const pbPackage = pbPackageType.create({
-            message_type: 'CEcho',
-            message_payload: wsEncoded
+        const pbPackage = commonProto.common.PBPackage.create({
+            messageType: 'CEcho',
+            messagePayload: wsEncoded
         });
-        const pbEncoded = pbPackageType.encode(pbPackage).finish();
+        const pbEncoded = commonProto.common.PBPackage.encode(pbPackage).finish();
 
         this.ws.send(pbEncoded);
         console.log(`发送回显请求 - 消息：${msg}`);
@@ -203,8 +166,6 @@ class TestClient {
  */
 async function startTest(): Promise<void> {
     const client = new TestClient('ws://localhost:8080');
-    
-    await client.loadProto();
     
     // 等待连接建立
     setTimeout(() => {
